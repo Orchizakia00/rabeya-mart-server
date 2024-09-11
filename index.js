e38614ece0c403de616309a1e2718c623866efdd
Tanzim Orchi <zakiaislam290@gmail.com>
2024-09-12 02:03:09 +0600

    cart validation in progress

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sdbndcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -52,7 +52,6 @@ async function run() {
       const result = await cartCollection.insertOne(data);
       res.send(result);
     })
-
 
     // for users
     app.get('/users', async (req, res) => {
